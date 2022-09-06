8b5ac8ae78841d1bbfaa7d9caefe619f68c5cbaf
Stephen <skairu09@gmail.com>
2022-09-06 18:10:29 +0300

    Fetch hair dye images from pexels

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@
 
 const container = document.querySelector(".container");
 let cardTag;
-function getPhotos(images) {
+function getHairPhotos(images) {
    images.map(image => {
      cardTag = `<div class="card">
               <img src=${image.src.large} />
@@ -26,6 +26,29 @@ function getPhotos(images) {
    })
 }
 fetch("https://api.pexels.com/v1/search?query=barbershop",{
+  headers: {
+    Authorization: "563492ad6f9170000100000183d0e0c8cced43af8b10aa4391e5752b"
+  }
+})
+   .then(resp => {
+     return resp.json()
+   })
+   .then(data => {
+     getHairPhotos(data.photos);
+   })
+
+
+   const hairDye = document.querySelector("#hair-dye");
+let dyeTag;
+function getPhotos(images) {
+   images.map(image => {
+     dyeTag = `<div class="card">
+              <img src=${image.src.large} />
+         </div>`;
+     hairDye.innerHTML += dyeTag;
+   })
+}
+fetch("https://api.pexels.com/v1/search?query=hairdye",{
   headers: {
     Authorization: "563492ad6f9170000100000183d0e0c8cced43af8b10aa4391e5752b"
   }
