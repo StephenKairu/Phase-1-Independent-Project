addf96427c7ab258f42ab23495a11a0470891269
Stephen <skairu09@gmail.com>
2022-09-08 01:19:51 +0300

    Add review posting feature

--- src/index.js
+++ src/index.js
@@ -1,15 +1,122 @@
+const container = document.querySelector("#container");
+let cardTag;
+function getHairPhotos(images) {
+   images.map(image => {
+     cardTag = `<div class="image-div">
+              <img src=${image.src.large} />
+              <p class="like">Like! <span class="like-glyph">&#x2661;</span></p>
+         </div>`;
+     container.innerHTML += cardTag;
+   })
+}
+fetch("https://api.pexels.com/v1/search?query=barbershop",{
+  headers: {
+    Authorization: "563492ad6f9170000100000183d0e0c8cced43af8b10aa4391e5752b"
+  }
+})
+   .then(resp => {
+     return resp.json()
+   })
+   .then(data => {
+     getHairPhotos(data.photos);
+   })
 
-// {/* <div class="container">
-// <!-- <div class="card">
-//     <img src="" />
-//  </div> --> 
-// </div> */}
 
-// // fetch("https://api.pexels.com/v1/search?query=people", {
-// //   headers: {
-// //     Authorization: '563492ad6f9170000100000183d0e0c8cced43af8b10aa4391e5752b'
-// //   }
-// // })
+   const hairDye = document.querySelector("#hair-dye");
+let dyeTag;
+function getPhotos(images) {
+   images.map(image => {
+     dyeTag = `<div class="image-div">
+              <img src=${image.src.large} />
+              <p class="like">Like! <span class="like-glyph">&#x2661;</span></p>
+         </div>`;
+     hairDye.innerHTML += dyeTag;
+   })
+}
+fetch("https://api.pexels.com/v1/search?query=hairdye",{
+  headers: {
+    Authorization: "563492ad6f9170000100000183d0e0c8cced43af8b10aa4391e5752b"
+  }
+})
+   .then(resp => {
+     return resp.json()
+   })
+   .then(data => {
+     getPhotos(data.photos);
+   })
+
+
+
+//    let EMPTY_HEART = '♡'
+//    let FULL_HEART = '♥'
+
+//    const errMessage = document.querySelector(".hidden");
+//    const emptyHeart  = document.querySelectorAll(".like-glyph");
+   
+//    emptyHeart.forEach((like) => {
+//      like.addEventListener("click", (e) => {
+//        if (like.innerHTML == EMPTY_HEART) {
+//          like.innerHTML = FULL_HEART;
+//          like.className = "activated-heart";
+//        } else if (like.innerText == FULL_HEART) {
+//          like.innerHTML = EMPTY_HEART;
+//        }
+//      });
+//    });
+
+
+function addReview(event) {
+  event.preventDefault()
+
+  let clientName= document.getElementById("client-name");
+  let clientText= document.getElementById("client-text");
+
+  let card = document.createElement('div')
+  card.className = 'card'
+  card.innerHTML = `
+  <div class="stars">
+      <i class="fas fa-star"></i>
+      <i class="fas fa-star"></i>
+      <i class="fas fa-star"></i>
+      <i class="fas fa-star"></i>
+      <i class="fas fa-star-half-alt"></i>
+  </div>
+  <h3>My Name is ${clientName.value}</h3>
+  <p>${clientText.value}
+  `
+  document.querySelector(".review-card").appendChild(card)
+}
+document.getElementById("comment-form").addEventListener("submit", addReview)
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    function renderOneReview(review) {
+      let card = document.createElement('div')
+      card.className = 'card'
+      card.innerHTML = `
+      <div class="stars">
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star-half-alt"></i>
+      </div>
+      <h3>My Name is ${review.name}</h3>
+      <p>${review.body}
+      `
+      document.querySelector(".review-card").appendChild(card)
+    }
+
+    function renderAllReviews() {
+      fetch('https://jsonplaceholder.typicode.com/posts/1/comments')
+        .then((response) => response.json())
+        .then((comments) => comments.forEach(review => renderOneReview(review)));
+      }
+    function init() {
+      renderAllReviews()
+    }
+    init()
+  })
 
 // //   .then(response => response.json())
 // //   .then(result => console.log(result))
@@ -40,139 +147,3 @@
 //    .then(data => {
 //      getEventType(data.photos);
 //    })
-
-
-
-
-
-
-
-
-// const container = document.querySelector("#container");
-// let cardTag;
-// function getHairPhotos(images) {
-//    images.map(image => {
-//      cardTag = `<div class="card">
-//               <img src=${image.src.large} />
-//               <p class="like">Like! <span class="like-glyph">&#x2661;</span></p>
-//          </div>`;
-//      container.innerHTML += cardTag;
-//    })
-// }
-// fetch("https://api.pexels.com/v1/search?query=barbershop",{
-//   headers: {
-//     Authorization: "563492ad6f9170000100000183d0e0c8cced43af8b10aa4391e5752b"
-//   }
-// })
-//    .then(resp => {
-//      return resp.json()
-//    })
-//    .then(data => {
-//      getHairPhotos(data.photos);
-//    })
-
-
-//    const hairDye = document.querySelector("#hair-dye");
-// let dyeTag;
-// function getPhotos(images) {
-//    images.map(image => {
-//      dyeTag = `<div class="card">
-//               <img src=${image.src.large} />
-//               <p class="like">Like! <span class="like-glyph">&#x2661;</span></p>
-//          </div>`;
-//      hairDye.innerHTML += dyeTag;
-//    })
-// }
-// fetch("https://api.pexels.com/v1/search?query=hairdye",{
-//   headers: {
-//     Authorization: "563492ad6f9170000100000183d0e0c8cced43af8b10aa4391e5752b"
-//   }
-// })
-//    .then(resp => {
-//      return resp.json()
-//    })
-//    .then(data => {
-//      getPhotos(data.photos);
-//    })
-
-
-
-//    let EMPTY_HEART = '♡'
-//    let FULL_HEART = '♥'
-
-//    const errMessage = document.querySelector(".hidden");
-//    const emptyHeart  = document.querySelectorAll(".like-glyph");
-   
-//    emptyHeart.forEach((like) => {
-//      like.addEventListener("click", (e) => {
-//        if (like.innerHTML == EMPTY_HEART) {
-//          like.innerHTML = FULL_HEART;
-//          like.className = "activated-heart";
-//        } else if (like.innerText == FULL_HEART) {
-//          like.innerHTML = EMPTY_HEART;
-//        }
-//      });
-//    });
-
-
-
-// const commentsAPI = "https://jsonplaceholder.typicode.com/comments?postId=1";
-// const customerName= document.querySelector(".customer-name");
-// const customerReview = document.querySelector(".customer-review")
-
-// fetch(commentsAPI)
-// .then((res) => res.json())
-// .then(renderReviews)
-
-// function renderReviews(data) {
-//   customerName.textContent = data.name;
-//   customerReview.textContent = data.body;
-// }
-
-// function postComments(comments) {
-//     commentsList.innerHTML = "";
-//     comments.forEach(postComment);
-// }
-// function postComment(comment) {
-//     const li = document.createElement("li");
-//     li.textContent = comment.content;
-//     commentsList.append(li);
-// }
-
-// function addComment(event) {
-//     event.preventDefault();
-//     const commentText = event.target.comment.value;
-//     postComment({content: commentText});
-//  event.target.reset();
-// }
-
-// document.getElementById("comment-form").addEventListener("submit", addComment)
-
-function renderOneReview(review) {
-  let card = document.createElement('div')
-  card.className = 'card'
-  card.innerHTML = `
-  <h3>My Name is ${review.name}</h3>
-  <p>${review.body}
-  `
-  document.querySelector(".review-card").appendChild(card)
-}
-
-
-function renderAllReviews() {
-fetch('https://jsonplaceholder.typicode.com/posts/1/comments')
-  .then((response) => response.json())
-  .then((comments) => comments.forEach(review => renderOneReview(review)));
-}
-
-function init() {
-  renderAllReviews()
-}
-init()
-
-  // function showComments(data) {
-  //   data.body
-  // }
-
-  // const customerReview = document.querySelector(".customer-review")
-  // customerReview.innerHTML = json.body;
